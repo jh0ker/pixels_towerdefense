cf63f5f0793fcbbb1e841ae5e158d50610c7e9a4
Jannes Höke <jhoeke@uni-bremen.de>
2015-05-18 15:54:20 +0200

    Lots of commenting

--- td.py
+++ td.py
@@ -1,9 +1,10 @@
-import pygame, led, sys, os, random
+import pygame, led, sys, os
 from pygame.locals import *
 
-""" A very simple arcade shooter demo :)
+""" https://github.com/jh0ker/pixels_towerdefense
 """
 
+# Colors
 BLACK = pygame.Color(0,0,0)
 WHITE = pygame.Color(255, 255, 255)
 GREEN = pygame.Color(0, 255, 0)
@@ -20,7 +21,6 @@ SHOOT = pygame.Color(50, 0, 0)
 # detect if a serial/USB port is given as argument
 hasSerialPortParameter = ( sys.argv.__len__() > 1 )
 
-# use 90 x 20 matrix when no usb port for real display provided
 fallbackSize = ( 90, 20 )
 
 if hasSerialPortParameter:
@@ -28,7 +28,7 @@ if hasSerialPortParameter:
     print "INITIALIZING WITH USB-PORT: "+serialPort
     ledDisplay = led.teensy.TeensyDisplay( serialPort, fallbackSize )
 else:
-    print "INITIALIZING WITH DisplayServerClientDisplay AND SIMULATOR."
+    print "INITIALIZING WITH SERVER DISPLAY AND SIMULATOR."
     ledDisplay = led.dsclient.DisplayServerClientDisplay('localhost', 8123, fallbackSize)
 
 # use same size for sim and real LED panel
@@ -50,27 +50,29 @@ life = 3
 
 score = 0.0
 
-
+# used for shooting ranges of towers
 def expandRect(rect, px):
     _rect = pygame.Rect(rect.x - px, rect.y - px, rect.width + px*2, rect.height + px*2);
     return _rect
 
+# Base class for Aliens
 class Animation:
     def linearMove(self, x, y):
         return self.rect.move(x, y)
 
+# The cursor to place towers and to select towers
 class Cursor(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         
         self.image = pygame.image.load(os.path.join('sprite', 'cursor.png'))
         self.rect = self.image.get_rect()
-        #self.rect = pygame.Rect((0,0), (2,2))
 
         self.rect.midleft = screen.get_rect().midleft
         self.rect.x = 2
 
     def move(self, x, y):
+        # Allow cursor to move out of screen by one pixel
         _rect = self.rect.move(x, y).clip(screen.get_rect())
         if _rect.width >= 3 and _rect.height >= 3 and _rect.x >= -1 and  _rect.y >= -1:
             self.rect = self.rect.move(x, y)
@@ -92,12 +94,14 @@ class Alien(pygame.sprite.Sprite, Animation):
         
         self.reduce_speed = 0
         
+        # Count how many times an alien was updated for slowing purposes
         self.updates = 0 
 
     def update(self, *args):
         
         self.updates += 1
         
+        # Allow a maximum of 75% speed reduce
         if self.reduce_speed > 2:
             self.reduce_speed = 2
         
@@ -108,6 +112,7 @@ class Alien(pygame.sprite.Sprite, Animation):
         
         self.reduce_speed = 0
         
+        # The path the alien walks 
         if self.rect.x <= 20 and self.rect.y == 9:
             _rect = self.linearMove(-1, 0)
         elif self.rect.x % 12 == 0:
@@ -125,7 +130,8 @@ class Alien(pygame.sprite.Sprite, Animation):
 
         self.rect = _rect
 
-    def kill(self, damage = 3.34):
+    # Override the kill method to implement a damage model
+    def kill(self, damage):
         
         global money, score
         self.hp -= damage
@@ -137,18 +143,20 @@ class Alien(pygame.sprite.Sprite, Animation):
         else:
             percent = float(self.hp) / self.maxhp
             
-            if percent < .35:
+            if percent < .33:
                 self.image.fill(RED)
-            elif percent < .68:
+            elif percent < .67:
                 self.image.fill(YELLOW)
             else:
                 self.image.fill(GREEN)
 
+# Subclass of alien with a different color which is used for the pathway
 class Path(Alien):
     def __init__(self):
         Alien.__init__(self)
         self.image.fill(pygame.Color(50, 50, 50))
-        
+
+# Tower classes    
 class Tower(pygame.sprite.Sprite):
     def __init__(self, cursor):
         pygame.sprite.Sprite.__init__(self)
@@ -216,15 +224,14 @@ class StrongestTower(Tower):
         t = pygame.sprite.Sprite()
         t.rect = shootrange
         
-        hits = 0
         for alien in aliens:
             targhit = pygame.sprite.collide_circle(t, alien)
             if targhit:
                 pygame.draw.line(screen, SHOOT, (self.rect.x + .5, self.rect.y + .5), (alien.rect.x + .5, alien.rect.y + .5), 1)
                 alien.kill(1.8)
                 break
-                #hits += 1
 
+# The Base which you should defend
 class Target(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -236,32 +243,13 @@ class Target(pygame.sprite.Sprite):
 def main():
     global alienSpeed, alienFrequency, alienHp, alienHpFactor, money, life, score
 
-    clock = pygame.time.Clock()
-    pygame.joystick.init()
-    
-    if pygame.joystick.get_count() > 0:
-        stick = pygame.joystick.Joystick(0)
-        stick.init()
-    else:
-        stick = None
-
-    cursor = Cursor()
-    target = Target()
-    
-    player = pygame.sprite.Group()
-    player.add(cursor)
-    player.add(target)
-
-    towers = pygame.sprite.Group()
-    aliens = pygame.sprite.OrderedUpdates()
-    
+    # (Re)set game variables
     towersel = 0
     
     movementX = 0
     movementY = 0
     lastAlien = 0
     
-    #reset game
     money = 6
     life = 3
     
@@ -272,11 +260,35 @@ def main():
     
     score = 0.0
     
+    tickcount = 0
+
+    # inits
+    clock = pygame.time.Clock()
+    pygame.joystick.init()
+    
+    # Initialize first joystick
+    if pygame.joystick.get_count() > 0:
+        stick = pygame.joystick.Joystick(0)
+        stick.init()
+        
+    # Stuff that belongs to the player
+    cursor = Cursor()
+    target = Target()
+    
+    player = pygame.sprite.Group()
+    player.add(cursor)
+    player.add(target)
+    
+    # Other sprite groups
+    towers = pygame.sprite.Group()
+    aliens = pygame.sprite.OrderedUpdates() # Make Aliens ordered so always the first one will be shot at
+    
     # Set up background
     bg = pygame.Surface(size)
     bg.fill(BLACK)
     bg.set_colorkey(BLACK)
     
+    # The path
     path = pygame.sprite.Group()
     
     for c in range(227):
@@ -287,6 +299,7 @@ def main():
         
     path.draw(bg)
     
+    # The Towers for selection and dots on the money-meter
     menu = pygame.sprite.Group()
     towcurs = Cursor()
     
@@ -308,11 +321,13 @@ def main():
     
     menu.draw(bg)
     
+    # Reset Tower Cursor for later use
     towcurs.move(-9, 0)
     player.add(towcurs)
     
     gameover = 0
     
+    # Show lobby message
     pygame.font.init()
     font_text = pygame.font.SysFont(None, 18)
     
@@ -330,17 +345,20 @@ def main():
         if event.type == KEYDOWN or event.type == JOYBUTTONDOWN:
             break
     
-    tickcount = 0
     
     while gameover < 2:
         gameover = 0
+        
+        # Process event queue
         for event in pygame.event.get():
             try:
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
-
+                
+                # Keypresses on keyboard and joystick axis motions / button presses
                 elif event.type == KEYDOWN or event.type == JOYAXISMOTION and event.value != 0 or event.type == JOYBUTTONDOWN:
+                    # Movements
                     if event.type == KEYDOWN and event.key == K_UP or event.type == JOYAXISMOTION and event.axis == 1 and event.value < 0:
                         movementY = -1
                     elif event.type == KEYDOWN and event.key == K_DOWN or event.type == JOYAXISMOTION and event.axis == 1 and event.value > 0:
@@ -350,6 +368,7 @@ def main():
                     elif event.type == KEYDOWN and event.key == K_LEFT or event.type == JOYAXISMOTION and event.axis == 0 and event.value < 0:
                         movementX = -1
                         
+                    # Tower selection
                     elif event.type == KEYDOWN and event.key == K_w or event.type == JOYBUTTONDOWN and event.button == 2:
                         towersel += 1
                         towcurs.move(3, 0)
@@ -357,6 +376,8 @@ def main():
                         if towersel > 3:
                             towersel = 0
                             towcurs.move(-12, 0)
+                            
+                    # Tower placement
                     elif event.type == KEYDOWN and event.key == K_SPACE or event.type == JOYBUTTONDOWN and event.button == 1:
                         if towersel == 0:
                             tower = Tower(cursor)
@@ -368,10 +389,6 @@ def main():
                             tower = StrongestTower(cursor)
                         
                         if money >= tower.cost:
-                            for t in towers:
-                                if pygame.sprite.collide_rect(t, tower):
-                                    t.kill()
-                            
                             place = True        
                             
                             for p in path:
@@ -379,33 +396,42 @@ def main():
                                     place = False
                             
                             if place:
+                                for t in towers:
+                                    if pygame.sprite.collide_rect(t, tower):
+                                        t.kill()
                                 towers.add(tower)
                                 money -= tower.cost
                                 
+                    # If both player buttons on the controller are pressed, end the game
                     elif event.type == JOYBUTTONDOWN and event.button == 7:
                         gameover += 1
                     elif event.type == JOYBUTTONDOWN and event.button == 8:
                         gameover += 1
 
+                # Stop cursor movement in case of keyup or axis move to home position
                 elif event.type == KEYUP or event.type == JOYAXISMOTION and event.value == 0.0:
                     if event.type == KEYUP and (event.key == K_UP or event.key == K_DOWN) or event.type == JOYAXISMOTION and event.axis == 1:
                         movementY = 0
                     elif event.type == KEYUP and (event.key == K_RIGHT or event.key == K_LEFT) or event.type == JOYAXISMOTION and event.axis == 0:
                         movementX = 0
                 
+            # We tried to process an event that we should not have processed
             except AttributeError:
                 print("AttributeError on " + str(event))
-                
+        
         cursor.move(movementX, movementY)
         
-        # increase difficulty
+        # Count ticks independently of time so the timings wont fuck up if the CPU is slow
         tickcount += 1
+        
+        # Spawn Alien and increase difficulty
         if (tickcount - lastAlien) > alienFrequency:
-            # spawn new alien :)
+            # Spawn new alien :)
             aliens.add(Alien(alienHp))
             lastAlien = tickcount
             alienHp *= alienHpFactor
             
+            # Some balancing stuff
             if alienSpeed > 3:
                 alienSpeed += alienSpeedAdd
             
@@ -421,7 +447,7 @@ def main():
                 
             print("Spawned Alien... HP: %6f HP Factor: %6f " % (alienHp, alienHpFactor))
 
-        # .. player hit?
+        # Alien arrived at Base?
         targhit = pygame.sprite.spritecollideany(target, aliens)
 
         if targhit != None:
@@ -431,8 +457,10 @@ def main():
         else:
             screen.fill(BLACK)
         
+        # Draw background
         screen.blit(bg, (0,0))
         
+        # Do updates on all groups
         player.update()
         
         if tickcount % alienSpeed == 0:
@@ -443,24 +471,29 @@ def main():
         
         towers.update(aliens)
         
+        # Draw all groups
         towers.draw(screen)
         aliens.draw(screen)
         player.draw(screen)
         
-        #draw money bar
+        # Draw money and life bar
         if money > 0:
             pygame.draw.line(screen, WHITE, (1, screen.get_rect().height - 2), (money, screen.get_rect().height - 2), 1)
             
         if life > 0:
             pygame.draw.line(screen, WHITE, (1, 1), (life, 1), 1)
         
+        # Update screen
         simDisplay.update(screen)
         ledDisplay.update(screen)
 
-        clock.tick(30)
         if life == 0:
             gameover = 2
     
+        # Tick the clock
+        clock.tick(30)
+        
+    # End of the game
     text_gameover = "GAME OVER"
     write_gameover = font_text.render(text_gameover, True, WHITE)
     
@@ -469,11 +502,13 @@ def main():
     simDisplay.update(screen)
     ledDisplay.update(screen)
     
+    # Wait for keypress
     while True:
         event = pygame.event.wait()
         if event.type == KEYDOWN or event.type == JOYBUTTONDOWN:
             break
             
+    # Show score
     screen.fill(BLACK)
     text_gameover = "Score: " + str(int(score))
     write_gameover = font_text.render(text_gameover, True, WHITE)
@@ -483,11 +518,12 @@ def main():
     simDisplay.update(screen)
     ledDisplay.update(screen)
     
+    # Wait for keypress
     while True:
         event = pygame.event.wait()
         if event.type == KEYDOWN or event.type == JOYBUTTONDOWN:
             break
-    
+    # Restart game
     main()
     
 main()
